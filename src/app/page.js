7f617ac10675970dfadd75a3acef966c1fabaf2e
f2tangf <f2tangf@gmail.com>
2025-06-19 23:37:42 +0800

    tarot update 4

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,10 @@
 'use client'; 
 
 import Image from "next/image";
-import tarot from '@/app/tarot/page'
+import Tarot from '@/app/tarot/page'
 
 export default function Home() {
-  return <RedirectToTarot />;
-
-  function RedirectToTarot() {
-    const router = useRouter();
-    useEffect(() => {
-      router.push('/tarot');
-    }, []);
-    return null;
-  }
+  return (
+    <Tarot />
+  );
 }
